4e26df629f5847d7125834b05d6a8b1f6051b53c
Kamal <designerkamal@gmail.com>
2017-05-21 20:01:05 +0530

    add Session listeners

--- Session.js
+++ Session.js
@@ -1,16 +1,38 @@
 import { NativeModules, NativeAppEventEmitter, Platform } from 'react-native';
 const SessionManager = NativeModules.OpenTokSessionManager;
 
-const listener = null;
+const listeners = [];
 
 export const connect = SessionManager.connect;
 export const sendMessage = SessionManager.sendMessage;
 
 export const onMessageRecieved = (callback) => {
-  listener = NativeAppEventEmitter.addListener(
+  listeners.push(NativeAppEventEmitter.addListener(
       'onMessageRecieved',
       (e) => callback(e)
-    );
+    ));
+};
+export const onSessionConnect = (callback) => {
+  listeners.push(NativeAppEventEmitter.addListener(
+      'onSessionConnect',
+      (e) => callback(e)
+    ));
+};
+export const onSessionDisconnect = (callback) => {
+  listeners.push(NativeAppEventEmitter.addListener(
+      'onSessionDisconnect',
+      (e) => callback(e)
+    ));
+};
+export const onConnectionFailed = (callback) => {
+  listeners.push(NativeAppEventEmitter.addListener(
+      'onConnectionFailed',
+      (e) => callback(e)
+    ));
 };
 
-export const stopListener = () => listener.remove();
+export const stopListener = () => {
+  for (var listener of listeners) {
+    listener.remove()
+  }
+};
